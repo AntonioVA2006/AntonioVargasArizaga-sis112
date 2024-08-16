d3c43b73b2e9fac3d3dd813769390249abaf29d5
AntonioVA2006 <antonio.vargas.a@ucb.edu.bo>
2024-08-15 21:35:21 -0400

    v2

--- codigo/calculadora.py
+++ codigo/calculadora.py
@@ -1,27 +1,47 @@
-print("CALCULADORA ¿Que opción desea usted?")
-print("1. Suma")
-print("2. Resta")
-print("3. Multiplicacion")
-print("4. Division")
-eleccion = int(input())
-if eleccion == 1:
-    num1 = int(input("Ingrese un numero "))
-    num2 = int(input("Ingrese otro numero "))
+def suma(num1, num2):
     result = num1+num2
-    print(num1, "+", num2, "=", result)
-elif eleccion == 2:
-    num1 = int(input("Ingrese un numero "))
-    num2 = int(input("Ingrese otro numero "))
-    result = num1 - num2
-    print(num1, "-", num2, "=", result)
-elif eleccion == 3:
-    num1 = int(input("Ingrese un numero "))
-    num2 = int(input("Ingrese otro numero "))
-    result = num1 * num2
-    print(num1, "x", num2, "=", result)
-elif eleccion == 4:
-    num1 = int(input("Ingrese un numero "))
-    num2 = int(input("Ingrese otro numero "))
-    result1 = num1 / num2
-    result = round(result1, 2)
-    print(num1, "/", num2, "=", result)
+    print (result)
+    seguir(result)
+def resta(num1, num2):
+    result = num1-num2
+    print (result)
+    seguir(result)
+def multiplicacion(num1, num2):
+    result = num1*num2
+    print (result)
+    seguir(result)
+def division(num1,num2):
+    result1 = num1/num2
+    result = round(result1,2)
+    print (result)
+    seguir(result)
+def seguir(result):
+    print("Desea seguir");
+    print("1. SI")
+    print("2. NO")
+    seguir = int(input())
+    if seguir == 1:
+        num1 = result
+        num2 = int(input("Ingrese otro numero "))
+        elec(num1, num2)
+    else:
+        print ("Gracias por su atencion")
+
+def elec(num1, num2):
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicacion")
+    print("4. Division")
+    eleccion = int(input())
+    if eleccion == 1:
+        suma(num1, num2)
+    elif eleccion == 2:
+        resta(num1, num2)
+    elif eleccion == 3:
+        multiplicacion(num1, num2)
+    elif eleccion == 4:
+        division(num1, num2)
+print("CALCULADORA")
+num1 = int(input("Inserte un numero "))
+num2 = int(input("Inserte otro numero "))
+elec(num1, num2)
