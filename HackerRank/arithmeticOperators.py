2b6ecc3bad34cbd0e9711c16eac6aaec1d61bf9d
AntonioVA2006 <antonio.vargas.a@ucb.edu.bo>
2024-08-16 19:07:13 -0400

    v1

---
+++ HackerRank/arithmeticOperators.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    suma = a+b
+    if a>b:
+        resta = a-b
+    else:
+        resta = b-a
+    mult = a*b
+    print(suma)
+    print(resta)
+    print(mult)
