8c3399867c14f2291c44b69ecaf518819f5131ce
AntonioVA2006 <antonio.vargas.a@ucb.edu.bo>
2024-09-11 19:14:31 -0400

    Version 1

---
+++ codigo/funciones_conversion.py
@@ -0,0 +1,18 @@
+def longitud1(num1):
+    num2 = num1/1000
+    return num2
+def longitud2(num1):
+    num2 = num1*1000
+    return num2
+def masa1(num1):
+    num2 = num1/1000
+    return num2
+def masa2(num1):
+    num2 = num1*1000
+    return num2
+def temperatura1(num1):
+    num2 = (num1*9/5)+32
+    return num2
+def temperatura2(num1):
+    num2 = (num1-32)*5/9
+    return num2
